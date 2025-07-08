6ee9eac184a90cbd754bd256c46202b571895350
heeeyram <136877946+heeeyram@users.noreply.github.com>
2025-07-09 04:47:18 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,166 @@
+// index.js
+export const servicesData = [
+  {
+    title: "FullStack Development",
+    description:
+      "Your business deserves a fast, secure, and future-proof digital foundation. I develop custom web apps with clean architecture, optimized databases, and seamless integrations—ensuring reliability at every layer.",
+    items: [
+      {
+        title: "Backend Engineering",
+        description: "(REST/GraphQL APIs, Microservices, Auth Systems)",
+      },
+      {
+        title: "Frontend Excellence",
+        description: "(React, Vue, TypeScript, Interactive UI/UX)",
+      },
+      {
+        title: "Database Design",
+        description: "(SQL/NoSQL Optimization, Scalable Structures)",
+      },
+    ],
+  },
+  {
+    title: "DevOps & Cloud Solutions",
+    description:
+      "Deploying software shouldn't be a gamble. I automate infrastructure, enforce security, and leverage cloud platforms (AWS/Azure) to keep your app running smoothly—24/7, at any scale.",
+    items: [
+      {
+        title: "CI/CD Pipelines",
+        description: "(GitHub Actions, Docker, Kubernetes)",
+      },
+      {
+        title: "Server Management ",
+        description: "(Linux, Nginx, Load Balancing)",
+      },
+      {
+        title: "Performance Tuning",
+        description: "(Caching, Compression, Lighthouse 90+ Scores)",
+      },
+    ],
+  },
+  {
+    title: "Security & Optimization",
+    description:
+      "Slow or hacked apps destroy trust. I harden security (XSS/SQLI protection, OAuth) and optimize bottlenecks so your app stays fast, safe, and scalable as you grow.",
+    items: [
+      {
+        title: "Code Audits",
+        description: "(Refactoring, Tech Debt Cleanup)",
+      },
+      {
+        title: "Pen Testing",
+        description: "(Vulnerability Assessments)",
+      },
+      {
+        title: "SEO Tech Stack",
+        description: "(SSR, Metadata, Structured Data)",
+      },
+    ],
+  },
+  {
+    title: "Web & Mobile Apps",
+    description:
+      "A clunky interface can sink even the best ideas. I craft responsive, pixel perfect web and mobile apps (React Native/Flutter) that users love—bridging design and functionality seamlessly.",
+    items: [
+      {
+        title: "Cross-Platform Apps",
+        description: "(Single codebase for iOS/Android/Web)",
+      },
+      {
+        title: "PWAs",
+        description: "(Offline mode, Push Notifications)",
+      },
+      {
+        title: "E-Commerce",
+        description: "(Checkout flows, Payment Gateways, Inventory APIs)",
+      },
+    ],
+  },
+];
+export const projects = [
+  {
+    id: 1,
+    name: "Yoom The Video Conferencing App",
+    description:
+      "A video conferencing app that offers high-quality video calls, screen sharing, and collaboration tools for remote teams.",
+    href: "https://video-confrencing-app-eight.vercel.app/",
+    image: "/assets/projects/yoom-video-conferencing.jpg",
+    bgImage: "/assets/backgrounds/blanket.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Next.js" },
+      { id: 3, name: "Node.js" },
+      { id: 4, name: "Tailwind CSS" },
+      
+    ],
+  },
+  {
+    id: 2,
+    name: "Zentry Redifining Gaming",
+    description:
+      "Zentry is a Zero Trust access platform designed to securely connect users—especially in gaming, media, and digital industries.",
+    href: "https://zentry-redifining-gaming.vercel.app/",
+    image: "/assets/projects/zentry.jpg",
+    bgImage: "/assets/backgrounds/curtains.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "GSAP" },
+      { id: 3, name: "Tailwind CSS" },
+      { id: 4, name: "Vercel" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Two Good Co.",
+    description:
+      "An e-commerce platform offering ethically made lifestyle products—from clothing to home goods—with a mission to support women experiencing hardship.",
+    href: "https://heeeyram.github.io/Two_Good_Co/",
+    image: "/assets/projects/Two-Good-co.jpg",
+    bgImage: "/assets/backgrounds/map.jpg",
+    frameworks: [
+      { id: 1, name: "HTML5" },
+      { id: 2, name: "CSS" },
+      { id: 3, name: "JavaScript" },
+      { id: 4, name: "GSAP" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Klimate the weather app",
+    description:
+      "A real-time weather app delivering accurate forecasts, severe weather alerts, and live radar for multiple locations worldwide.",
+    href: "",
+    image: "/assets/projects/klimate.jpg",
+    bgImage: "/assets/backgrounds/poster.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Typescript" },
+      { id: 3, name: "OpenWeatherMap API" },
+      { id: 4, name: "Vercel" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Brainwave",
+    description:
+      "An AI-powered, visually engaging landing page built for a SaaS product, featuring modern UI animations, responsive layouts, and product highlights.",
+    href: "",
+    image: "/assets/projects/brainwave.jpg",
+    bgImage: "/assets/backgrounds/table.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Tailwind CSS" },
+      { id: 3, name: "Framer Motion" },
+      { id: 4, name: "GitHub Pages" },
+    ],
+  },
+  
+];
+export const socials = [
+  { name: "Instagram", href: "https://www.instagram.com/heeey.ram/" },
+  {
+    name: "GitHub",
+    href: "https://github.com/heeeyram",
+  },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/ramratangupta1/" },
+];
---
+++ src/sections/About.jsx
@@ -0,0 +1,63 @@
+import { useRef } from "react";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { AnimatedTextLines } from "../components/AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const About = () => {
+  const text = `Passionate about clean architecture
+    I build scalable, high-performance solutions
+    from prototype to production`;
+  const aboutText = `Driven to craft clean, efficient code—whether it's seamless React UIs or robust full-stack logic. Every project is a step toward digital elegance users can trust.
+  When I’m not coding:
+⚙️ Building personal tools or contributing to open-source communities
+🧠 Learning new stacks, experimenting with ideas, and breaking stuff to grow
+🚶‍♂️ Exploring Delhi’s streets, chasing inspiration one walk at a time
+🎧 Vibing to indie tracks while my terminal runs without warnings`;
+  const imgRef = useRef(null);
+  useGSAP(() => {
+    gsap.to("#about", {
+      scale: 0.95,
+      scrollTrigger: {
+        trigger: "#about",
+        start: "bottom 80%",
+        end: "bottom 20%",
+        scrub: true,
+        markers: false,
+      },
+      ease: "power1.inOut",
+    });
+
+    gsap.set(imgRef.current, {
+      clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
+    });
+    gsap.to(imgRef.current, {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      duration: 2,
+      ease: "power4.out",
+      scrollTrigger: { trigger: imgRef.current },
+    });
+  });
+  return (
+    <section id="about" className="min-h-screen bg-black rounded-b-4xl">
+      <AnimatedHeaderSection
+        subTitle={"Cod with purpose, Built to scale"}
+        title={"About"}
+        text={text}
+        textColor={"text-white"}
+        withScrollTrigger={true}
+      />
+      <div className="flex flex-col items-center justify-between gap-16 px-10 pb-16 text-xl font-light tracking-wide lg:flex-row md:text-2xl lg:text-3xl text-white/60">
+        <img
+          ref={imgRef}
+          src="images/man.png"
+          alt="man"
+          className="w-md rounded-3xl"
+        />
+        <AnimatedTextLines text={aboutText} className={"w-full"} />
+      </div>
+    </section>
+  );
+};
+
+export default About;
